b424f92896d4e45c37d64a60c6a90dcc984074fc
Mohamed Sallam <74076841+MohamedSllaam@users.noreply.github.com>
2026-07-25 23:32:00 +0300

    Time: 521 ms (17.71%), Space: 0B (100%) - LeetHub

---
+++ 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
@@ -0,0 +1,90 @@
+
+
+
+----WITH MaxProducts AS
+----(
+----    SELECT
+----        i.store_id,
+----        i.product_name,
+----        i.price,
+----		i.quantity
+----    FROM inventory i
+----    WHERE i.price = (
+----        SELECT MAX(i2.price)
+----        FROM inventory i2
+----        WHERE i2.store_id = i.store_id
+----    ) and
+----	(
+----        SELECT count(i2.product_name)
+----        FROM inventory i2
+----        WHERE i2.store_id = i.store_id
+----    ) >=3
+--),
+-- MinProducts AS
+--(
+--select  i.store_id ,i.product_name ,i.quantity,i.price 
+--from inventory i
+--where  i.price =(select min(i2.price) from inventory i2 
+--where i2.store_id =  i.store_id )
+--and
+--	(
+--        SELECT count(i2.product_name)
+--        FROM inventory i2
+--        WHERE i2.store_id = i.store_id
+--    ) >=3
+--)
+
+--SELECT mx.store_id ,s.store_name,s.location ,mx.product_name most_exp_product ,
+--mi.product_name cheapest_product ,  
+--CAST(ROUND(1.0 * mi.quantity / mx.quantity, 2) AS DECIMAL(10,2)) imbalance_ratio from
+--MaxProducts mx join MinProducts mi
+--on  mx.store_id = Mi.store_id
+--join stores s
+-- on mx.store_id = s.store_id
+-- order by imbalance_ratio desc , store_name asc
+
+
+
+
+
+WITH MaxProducts AS
+(
+    SELECT
+        i.store_id,
+        i.product_name,
+        i.price,
+		i.quantity
+    FROM inventory i
+    WHERE i.price = (
+        SELECT MAX(i2.price)
+        FROM inventory i2
+        WHERE i2.store_id = i.store_id
+    )  
+)
+,
+ MinProducts AS
+(
+select  i.store_id ,i.product_name ,i.quantity,i.price  
+from inventory i
+where  i.price =(select min(i2.price) from inventory i2 
+where i2.store_id =  i.store_id )
+)
+
+SELECT mx.store_id ,s.store_name,s.location ,mx.product_name most_exp_product ,
+mi.product_name cheapest_product , 
+CAST(ROUND(1.0 * mi.quantity / mx.quantity, 2) AS DECIMAL(10,2)) imbalance_ratio
+
+from
+MaxProducts mx join MinProducts mi
+on  mx.store_id = Mi.store_id
+join stores s
+ on mx.store_id = s.store_id
+ join  inventory iin
+ on mx.store_id = iin.store_id
+ where mx.quantity < mi.quantity
+ group by 
+ mx.store_id ,s.store_name,s.location ,mx.product_name  ,
+mi.product_name  , 
+CAST(ROUND(1.0 * mi.quantity / mx.quantity, 2) AS DECIMAL(10,2)) 
+  having count( iin.store_id) >=3
+ order by imbalance_ratio desc , store_name asc
